decc600749fba37ea6d94669316eba1afa3a6062
Francesco Biscani <bluescarni@gmail.com>
2022-09-11 20:19:01 +0200

    Try changing the generator on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake ^
-    -G "NMake Makefiles" ^
+    -G "Visual Studio 16 2019" -A x64 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
